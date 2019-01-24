959f8dac9fc4313f471bc3083ab8e542c15c7d1d
Matt Weiss <weiss.matt@outlook.com>
2019-01-23 21:20:03 -0700

    add test_it_can_count_correct_answers_by_category

--- test/round_test.rb
+++ test/round_test.rb
@@ -73,7 +73,7 @@ class RoundTest < Minitest::Test
   end
 
   def test_it_can_count_correct_answers_by_category
-    skip
+
     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
@@ -85,7 +85,7 @@ class RoundTest < Minitest::Test
     round.take_turn("Juneau")
     round.take_turn("Mars")
 
-
+assert_equal 1, round.number_correct_by_category(:STEM)
 
   end
 
